f89ab9bbc94cca56a3e69373625385d6626661e8
SP <spsinghrathore1807@gmail.com>
2024-09-05 16:48:06 +0530

    change titles

--- src/main.js
+++ src/main.js
@@ -3,4 +3,17 @@ import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
+import '../styles/components/marquee.css';
+import '../styles/components/classes.css';
+import '../styles/components/contact.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
 import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+
+
+mobileNav();
+darkMode();
+
